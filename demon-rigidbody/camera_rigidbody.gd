11f72453298a1b5d62284e45bd66f164a27c5b49
Samuelms <samuel.sarimo@gmail.com>
2025-05-18 18:05:25 +0300

    Samuelin jutut lisätty

---
+++ demon-rigidbody/camera_rigidbody.gd
@@ -0,0 +1,13 @@
+extends Camera3D
+
+@export var follow_speed: float = 5.0
+@export var target: RigidBody3D
+@export var target_offset_z: float = 5.0
+@export var target_offset_y: float = 5.0
+
+func _process(delta: float) -> void:
+	if target:
+		# Calculate target position (only X and Z from target, keep camera's current Y)
+		var target_pos = Vector3(target.global_position.x, target.global_position.y - target_offset_y, target.global_position.z - target_offset_z)
+		# Move smoothly toward target position
+		global_position = global_position.lerp(target_pos, follow_speed * delta)
---
+++ demon-rigidbody/demon.gd
@@ -0,0 +1,68 @@
+extends RigidBody3D
+@export var move_speed: float = 5.0
+@export var turn_speed: float = 1.0
+@export var ground_offset: float = 0
+@export var sprint_speed: float = 10.0
+@export var tilt_speed: float = 30
+@export var mass_offset_speed = 0.1
+@onready var left_target: Marker3D = $leftLegIKTarget
+@onready var right_target: Marker3D = $rightLegIKTarget
+@onready var tilt_target: Marker3D = $Armature/Skeleton3D/bodyTiltTarget
+@onready var body_tilt: LookAtModifier3D = $Armature/Skeleton3D/bodyTilt
+@onready var collision: CollisionShape3D = $collision
+
+@export var tilt_limit_right: float = 6.4
+@export var tilt_limit_left: float = -3.7
+var atLimit: bool = false
+@onready var physical_bone_simulator_3d: PhysicalBoneSimulator3D = $Armature/Skeleton3D/PhysicalBoneSimulator3D
+
+	
+### testing tilt manually
+func _handle_tilt(delta: float):
+	if Input.is_action_pressed("tilt_left"):
+		if tilt_target.position.y < tilt_limit_left:
+			tilt_target.position.y = tilt_limit_left
+			atLimit = true
+		else:
+			tilt_target.position.y -= tilt_speed * delta
+			#center_of_mass.x -= mass_offset_speed
+			atLimit = false
+		
+	if Input.is_action_pressed("tilt_right"):
+		if tilt_target.position.y > tilt_limit_right:
+			tilt_target.position.y = tilt_limit_right
+			atLimit = true
+			
+		else:
+			tilt_target.position.y += tilt_speed * delta
+			#center_of_mass.x += mass_offset_speed
+			atLimit = false
+	
+	if atLimit:
+		freeze = false # Turn on physics
+		#physical_bone_simulator_3d.physical_bones_start_simulation()
+		return
+	
+	#physical_bone_simulator_3d.physical_bones_stop_simulation()
+	freeze = true
+
+
+func _handle_movement(delta: float):
+	var current_speed = move_speed
+	
+	var dir = Input.get_axis("up", "down")
+	translate(Vector3(0, 0, dir) * move_speed * delta)
+	
+	var a_dir = Input.get_axis("right", "left")
+	rotate_object_local(Vector3.UP, a_dir * turn_speed * delta)
+		
+	
+
+func _physics_process(delta: float) -> void:
+	if freeze:
+		var avg = (left_target.position + right_target.position) / 2
+		var target_pos = avg + transform.basis.y * ground_offset
+		var distance = transform.basis.y.dot(target_pos - position)
+		position = lerp(position, position + transform.basis.y * distance, move_speed * delta)
+		_handle_movement(delta)
+	_handle_tilt(delta)
